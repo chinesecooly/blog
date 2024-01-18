c14849637a4ed99d55e363e8ee1699862c700b36
chinesecooly <chinesecooly@163.com>
2024-01-18 15:14:30 +0800

    test

--- src/main/java/ink/ningyu/blog/controller/TestController.java
+++ src/main/java/ink/ningyu/blog/controller/TestController.java
@@ -21,7 +21,7 @@ public class TestController {
     @GetMapping("/log")
     public Result log() {
         Test test = new Test();
-        test.setMsg("a get request after two push");
+        test.setMsg("a get request after three push");
         test.setData(LocalDateTime.now());
         testMapper.insert(test);
         return Result.success(test);
