46cf5b01c62a83d382fabfffe7c1679025a001d8
chinesecooly <chinesecooly@163.com>
2024-02-09 20:48:56 +0800

    更改前端配置

---
+++ client/b-client/playground/.nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/Administrator/Desktop/blog/client/b-client/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ client/b-client/playground/.nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/Administrator/Desktop/blog/client/b-client/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
