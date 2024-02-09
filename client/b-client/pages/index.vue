39530b1caf0561e92ab5eca5a53a5e5e273e8757
chinesecooly <chinesecooly@163.com>
2024-02-09 21:31:48 +0800

    修改前端

--- client/b-client/pages/index.vue
+++ client/b-client/pages/index.vue
@@ -5,5 +5,16 @@
 <template>
     <div>
         <h1>宁泽龙王八蛋，说好的一起打牌，结果自己跑了，这是人干的事吗？</h1>
+        <img src="../assets/chetiefucktupian-40131859_3.jpg" />
     </div>
 </template>
+
+<style scoped>
+div {
+    text-align: center;
+}
+
+h1 {
+    text-align: center;
+}
+</style>
